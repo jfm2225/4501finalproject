568db72ac56ccd0ef56752f22c5ab1beff45d79b
John Musgrove <116607715+jfm2225@users.noreply.github.com>
2022-11-28 15:02:27 -0500

    Add files via upload

---
+++ schema.sql
@@ -0,0 +1,33 @@
+CREATE TABLE IF NOT EXISTS hourlyWeather (
+id INTEGER PRIMARY KEY,
+date DATETIME,
+HourlyPrecipitation FLOAT,
+HourlyWindSpeed FLOAT
+);
+
+CREATE TABLE IF NOT EXISTS dailyWeather (
+id INTEGER PRIMARY KEY,
+date DATE,
+DailyPrecipitation FLOAT,
+DailyWindSpeed FLOAT
+);
+
+CREATE TABLE IF NOT EXISTS taxi (
+id INTEGER PRIMARY KEY,
+pickup_datetime DATETIME,
+pickup_longitude FLOAT,
+pickup_latitude FLOAT,
+dropoff_longitude FLOAT,
+dropoff_latitude FLOAT,
+distance FLOAT
+);
+
+CREATE TABLE IF NOT EXISTS uber (
+id INTEGER PRIMARY KEY,
+pickup_datetime DATETIME,
+pickup_longitude FLOAT,
+pickup_latitude FLOAT,
+dropoff_longitude FLOAT,
+dropoff_latitude FLOAT,
+distance FLOAT
+);
